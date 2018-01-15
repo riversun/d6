989fac4487e6afaeda7bb2c629753f7506016ea2
Tom Misawa <riversun.org@gmail.com>
2018-01-15 10:04:31 +0900

    Update DB connection settings for test

--- src/test/java/org/riversun/d6/test/prepare_test/TestHelper_TestDBDef.java
+++ src/test/java/org/riversun/d6/test/prepare_test/TestHelper_TestDBDef.java
@@ -9,28 +9,30 @@ import org.riversun.d6.DBConnInfo;
  */
 public class TestHelper_TestDBDef {
 
-    // EDIT HERE FOR YOUR ENVIRONMENT
-    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";//"com.mysql.jdbc.Driver";
+  // EDIT HERE FOR YOUR ENVIRONMENT
+  public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";// "com.mysql.jdbc.Driver";
 
-    // EDIT HERE FOR YOUR ENVIRONMENT
-    public static final String DATABASE_URL = "jdbc:mysql://localhost/";
+  // EDIT HERE FOR YOUR ENVIRONMENT
+  public static final String DATABASE_URL = "jdbc:mysql://localhost/";
 
-    // EDIT HERE FOR YOUR ENVIRONMENT
-    public static final String USER = "[USERNAME_OF_THE_DB]";
+  // EDIT HERE FOR YOUR ENVIRONMENT
+  public static final String USER = "root";
 
-    // EDIT HERE FOR YOUR ENVIRONMENT
-    public static final String PASS = "[PASSWORD_OF_THE_DB]";
+  // EDIT HERE FOR YOUR ENVIRONMENT
+  public static final String PASS = "password";
 
-    public static final String TEST_DATABASE_NAME = "d6_test_db";
+  public static final String OPTION = "?characterEncoding=UTF-8&serverTimezone=JST";
 
-    public static DBConnInfo createTestDBConnInfo() {
-        DBConnInfo dbConnInfo = new DBConnInfo();
+  public static final String TEST_DATABASE_NAME = "d6_test_db";
 
-        dbConnInfo.DBDriver = JDBC_DRIVER;
-        dbConnInfo.DBUrl = DATABASE_URL + TEST_DATABASE_NAME;
-        dbConnInfo.DBUser = USER;
-        dbConnInfo.DBPassword = PASS;
+  public static DBConnInfo createTestDBConnInfo() {
+    DBConnInfo dbConnInfo = new DBConnInfo();
 
-        return dbConnInfo;
-    }
+    dbConnInfo.DBDriver = JDBC_DRIVER;
+    dbConnInfo.DBUrl = DATABASE_URL + TEST_DATABASE_NAME + OPTION;
+    dbConnInfo.DBUser = USER;
+    dbConnInfo.DBPassword = PASS;
+
+    return dbConnInfo;
+  }
 }
